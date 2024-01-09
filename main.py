15cf742effb1f8f0c585fbd9773305d93366f64b
Hoon <a32252613@gmail.com>
2024-01-09 12:02:17 +0900

    .

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-a = 3
-print(a)
+import pygame
+import win32api
+import win32con
+import win32gui
+import ctypes
+
+pygame.init()
+
+user32 = ctypes.windll.user32
+screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)  # 해상도 구하기
+screen = pygame.display.set_mode(screensize, pygame.NOFRAME)
+done = False
+fuchsia = (255, 0, 128)  # Transparency color
+dark_red = (139, 0, 0)
+
+win32gui.SetWindowPos(pygame.display.get_wm_info()['window'], win32con.HWND_TOPMOST, 0,0,0,0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+
+
+# Create layered window
+hwnd = pygame.display.get_wm_info()["window"]
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
+                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+# Set window transparency color
+win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(fuchsia)  # Transparent background
+    pygame.draw.rect(screen, dark_red, pygame.Rect(30, 30, 60, 60))
+    pygame.display.update()
